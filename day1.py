0813f1d86180bfc467ebc9dd480924375b21448e
Nandhakumarmethavi <119517662+Nandhakumarmethavi@users.noreply.github.com>
2023-03-22 19:24:41 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,37 @@
+'''print("hello world")
+
+#print on different line
+print("hello world\nhello world\nhello world")
+
+#combing of strings
+print("hello"+"world1")
+
+#space b/w string
+print("hello"+" "+"world2")
+
+#input from use
+input("whats your name")
+
+#printing inside print statement
+print("hello"+input("enter your name"))
+
+#counting len of string taking input from user
+print(len(input("enter your name")))
+
+#creating variable
+name=input("enter your name")
+len=len(name)
+print(len)
+
+#interchange values of variable
+a=10
+b=5
+c=a
+a=b
+b=c
+print(a,b)'''
+
+#variable naming
+#no space in b/w name instead use _ underscore
+#name cannot start with a number
+
